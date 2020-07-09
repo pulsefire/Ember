339c97a26febcaacc41dc6d4e8c65c6dce942c1c
unknown <marknovose@gmail.com>
2020-07-09 03:59:15 +0100

    Maintenance

---
+++ Ember/vendor/spdlog/obj/Windows/Release/fmt.d
@@ -0,0 +1,10 @@
+../obj/Windows/Release/fmt.o: ../src/fmt.cpp \
+ ../include/spdlog/fmt/bundled/format-inl.h \
+ ../include/spdlog/fmt/bundled/format.h \
+ ../include/spdlog/fmt/bundled/core.h
+
+../include/spdlog/fmt/bundled/format-inl.h:
+
+../include/spdlog/fmt/bundled/format.h:
+
+../include/spdlog/fmt/bundled/core.h:
