964cc32d3e07d4202ff9fd7f002880e952a7b04b
unknown <marknovose@gmail.com>
2020-07-19 11:54:10 +0100

    Maintenance Update

--- obj/Debug-x86_64/Ember/glad.d
+++
@@ -1,7 +0,0 @@
-../obj/Debug-x86_64/Ember/glad.o: ../Ember/vendor/glad/src/glad.c \
- ../Ember/vendor/glad/include/glad/glad.h \
- ../Ember/vendor/glad/include/KHR/khrplatform.h
-
-../Ember/vendor/glad/include/glad/glad.h:
-
-../Ember/vendor/glad/include/KHR/khrplatform.h:
